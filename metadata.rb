fd99d1fc586b124e0ba8158762fc0adca786faf2
Irving Popovetsky <irving@chef.io>
2017-04-11 08:47:16 -0700

    Bump major version because we're officially breaking compat with Chef 11 to support Chef 13

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,6 @@ description 'Overrides the execute resource to force live_streaming of output'
 long_description 'Overrides the execute resource to force live_streaming of output'
 source_url 'https://github.com/irvingpop/fancy_execute'
 issues_url 'https://github.com/irvingpop/fancy_execute/issues'
-version '1.0.2'
+version '2.0.0'
 
 chef_version '>= 12.4' if respond_to?(:chef_version)
